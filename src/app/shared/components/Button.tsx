d2d6a4ce8abb46bfd4fe9ad88c1f1ad35ed04404
lvsouza <ls18017@gmail.com>
2021-02-13 12:23:00 -0300

    feat: added shared button component

---
+++ src/app/shared/components/Button.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+    variant?: 'contained' | 'outlined' | 'text';
+}
+export const Button: React.FC<ButtonProps> = ({ variant, ...props }) => {
+    if (variant === 'outlined') return (
+        <button
+            {...props}
+            className={"padding-s margin-top-m background-transparent border-radius-soft font-size-m text-uppercase border-thin " + props.className}
+        />
+    );
+
+    if (variant === 'text') return (
+        <button
+            {...props}
+            className={"padding-s margin-top-m background-transparent border-radius-soft font-size-m text-uppercase border-none " + props.className}
+        />
+    );
+
+    return (
+        <button
+            {...props}
+            className={"padding-s margin-top-m background-primary border-radius-soft border-none text-white font-size-m text-uppercase " + props.className}
+        />
+    );
+}

19d4b14cec67648cd1d628c00b8b5b7db1f2abf9
lvsouza <ls18017@gmail.com>
2021-03-13 11:03:19 -0300

    feat: added dashboard page

---
+++ src/app/pages/dashboard/Dashboard.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { DarkModeCheckbox } from '../../shared/components/dark-mode-checkbox/DarkModeCheckbox';
+import './Dashboard.css';
+
+export const Dashboard: React.FC = () => {
+    return (
+        <div className="dashboard-base flex-content-center flex-items-center">
+            lfghsdkfljhglkjshdfkjghslkjdfhgfdg
+            sdf
+            gs
+            dfg
+            sd
+            fg
+            sdfg
+
+            <DarkModeCheckbox />
+        </div>
+    );
+}
--- src/app/routes/Routes.tsx
+++ src/app/routes/Routes.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 
+import { Dashboard } from '../pages/dashboard/Dashboard';
 import { Signin } from '../pages/signin/Signin';
 import { Signup } from '../pages/signup/Signup';
 
@@ -11,6 +12,8 @@ export const Routes: React.FC = () => {
             <Switch>
                 <Route exact path="/signin" component={Signin} />
                 <Route exact path="/signup" component={Signup} />
+
+                <Route exact path="/dashboard" component={Dashboard} />
 
                 <Route exact path="*" component={() => <Redirect to="/signin" />} />
             </Switch>
